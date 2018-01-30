3d62311bf831d014471c98552cc179b648517cc2
Philly Tan <phillberttan@gmail.com>
2018-01-30 10:35:58 +0800

    Added option to delete

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,21 +1,25 @@
 var bodyParser = require("body-parser");
-
-var data = [{item:'Entry 12'}, {item:'Entry 2'}, {item:'Entry 3'}];
-var urlencodedParser = bodyParser.urlencoded({extended:false});
+var idCount = 2;
+var data = [{id: 0, item:'Entry 12'}, {id: 1, item:'Entry 2'}, {id: 2, item:'Entry 3'}];
+var urlencodedParser = bodyParser.urlencoded({extended:true});
 
 module.exports = function(app){
 
-	app.get("/todo",function(req,res){
+	app.get("/",function(req,res){
 	  res.render("todo",{todos:data});
 	});
 
-	app.post("/todo",urlencodedParser,function(req,res){
-		data.push(req.body);
-		res.json(data);
+	app.post("/",urlencodedParser,function(req,res){
+		data.push({item: req.body.item, id: idCount+1});
+		idCount++;
+		res.redirect('/');
 	});
 
-	app.delete("/todo",function(req,res){
-
+	app.get("/delete/:id",function(req,res){
+		data = data.filter(arr => {
+			if (arr.id != req.params.id) return true;
+		});
+		res.redirect('/');
 	});
 
 };
